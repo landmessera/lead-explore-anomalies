0b150f8afe2b28700af9b5ae22db4377c442023a
EfosaDO <120370666+EfosaDO@users.noreply.github.com>
2023-10-23 13:07:41 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,130 @@
+import os
+import pickle
+import pandas as pd
+import numpy as np
+import datetime as dt
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+import streamlit as st
+from utils import helper
+###########################################################################
+# Settings
+###########################################################################
+analyse_timerange = (dt.date(2023, 8, 1), dt.date(2023, 9, 1))
+
+# Kuba
+machineId = "603cd4524c34d5594f30cef1"
+machineName = "Kuba"
+
+# Borkum
+#machineId = "603cd3ac4c34d5fac730ced3"
+#machineName = "Borkum"
+
+res_path = os.path.join(os.getcwd(), 'results')
+
+legend_activated=True
+
+with open('settings_'+machineId+'.pickle', 'rb') as handle:
+    settings = pickle.load(handle)
+
+sensor_labels = settings['sensor_name_mapping']
+sensor_labels_ext = {}
+for key, label in sensor_labels.items():
+    sensor_labels_ext['sensor_'+key] = label
+
+st.set_page_config(layout="wide")
+###########################################################################
+# Load Data
+###########################################################################
+
+# get all files from results directory
+loaded_dfs = []
+list_filenames = []
+for filename in os.listdir(res_path):
+    list_filenames.append(filename)
+
+filenames_filtered = []
+for filename in list_filenames:
+    valid = False
+
+    # filter by analyse timerange
+    file_timerange = (dt.datetime.strptime(filename.split('_')[1],"%Y-%m-%d").date(), dt.datetime.strptime(filename.split('_')[2],"%Y-%m-%d").date())
+    if analyse_timerange[0] > file_timerange[0] or analyse_timerange[1] < file_timerange[1]:
+        continue
+
+    # filter by machine id
+    if machineId not in filename:
+        continue
+
+    filenames_filtered.append(filename)
+
+
+# todo if necessary
+list_filenames_sorted = filenames_filtered.copy()
+
+# parse dataframes from pickle files
+for filename in list_filenames_sorted:
+    with open(os.path.join(res_path,filename), 'rb') as handle:
+        ldf = pickle.load(handle)
+        loaded_dfs.append(ldf)
+
+for ldf in loaded_dfs:
+    print(ldf.shape)
+
+# concatenate all loaded dataframes
+df = pd.concat(loaded_dfs)
+
+
+
+###########################################################################
+# Split Data
+###########################################################################
+df.loc[df['cluster'] != 'increase','cluster'] = 0
+df.sort_index(inplace=True)
+sensor_columns = [c for c in df.columns if isinstance(c, str) and 'sensor' in c]
+station_columns = [column for column in df.columns if isinstance(column,str) and 'sensor' not in column and 'total' not in column and 'total_parts' not in column and 'cluster' not in column]
+changes = helper.detect_changes(df, 'cluster')
+anomaly_ranges = helper.anomaly_ranges(changes, 'increase')
+
+anomalies = {}
+for anomaly_range in anomaly_ranges:
+    start = anomaly_range[0] - pd.Timedelta(hours=3)
+    end = anomaly_range[1] + pd.Timedelta(hours=6)
+    anomalies[anomaly_range] = df[start:end]
+
+###########################################################################
+# Visualization
+###########################################################################
+print('Visualizing...')
+st.title("Anomalies in Failures: "+str(analyse_timerange[0])+" - "+str(analyse_timerange[1])+" "+machineName)
+st.write("The anomalies were detected using machine learning algorithms. The Kmeans clustering algorithm and the Dynamic Time Warping metric were applied. Each row shows one anomaly. The first graph shows the total failures (rejects) in relation to the total production volume (in percent). The second graph shows the distribution of the failures among the sensors. The third graph shows the distribution of failures among stations.")
+
+col1, col2, col3 = st.columns(3)
+for anomaly_range, dfa in anomalies.items():
+    if dfa.loc[anomaly_range[1]]['total'] < 2.0:
+        continue
+
+    with col1:
+        # Failures total and clusters
+        fig = px.line(dfa['total'], title=str(anomaly_range[0].date())+" "+str(anomaly_range[0].time())+": detected anomaly (red)")
+        fig = helper.highlight_anomaly(fig, anomaly_range)
+        #fig = position_legend(fig, show_legend=False)
+        st.plotly_chart(fig, theme='streamlit', use_container_width=True)
+
+    with col2:
+        # Sensors Stacked Barchart
+        fig = px.bar(dfa[sensor_columns], title="Failures by Sensors")
+        fig = helper.change_labels(fig, sensor_labels_ext)
+        #fig = position_legend(fig, show_legend=legend_activated)
+        st.plotly_chart(fig, theme='streamlit', use_container_width=True)
+
+    with col3:
+        # Failures by station
+        fig = px.line(dfa[station_columns], title="Failures by Stations")
+        #fig = position_legend(fig, show_legend=legend_activated)
+        st.plotly_chart(fig, theme='streamlit', use_container_width=True)
+
+
+
+
---
+++ utils/helper.py
@@ -0,0 +1,77 @@
+###########################################################################
+# Helper Functions
+###########################################################################
+
+def anomaly_ranges(changes, by_cluster):
+    anomaly_ranges = []
+    for idx, change in enumerate(changes):
+        if change[1] == 'increase':
+            anomaly_ranges.append((change[0], changes[idx+1][0]))
+
+    return anomaly_ranges
+
+def detect_changes(df, column):
+    current = ''
+    changes = []
+
+    for ind in df.index:
+        if current != df[column][ind]:
+            current = df[column][ind]
+            changes.append([ind, df[column][ind]])
+
+    return changes
+
+def highlight_cluster(fig, df, changes):
+    # Highlight areas according to the cluster values
+    for i, val in enumerate(changes):
+        if (i + 1) == len(changes):
+            changes_last_date = changes[-1][0]
+            changes_last_state = changes[-1][1]
+            last_date = df.index[-1]
+            fig.add_vrect(
+                x0=changes_last_date, x1=last_date,
+                fillcolor='white', opacity=0.2,
+                layer="below", line_width=0)
+
+        else:
+            if changes[i][1] == 'increase':
+                fig.add_vrect(
+                    x0=changes[i][0], x1=changes[i + 1][0],
+                    fillcolor='red', opacity=0.2,
+                    layer="below", line_width=0)
+
+    fig.update_layout(xaxis_title="Date", yaxis_title="Values",
+                      xaxis={'showgrid': False},
+                      yaxis={'showgrid': True})
+    return fig
+
+def highlight_anomaly(fig, anomaly_range):
+    fig.add_vrect(
+            x0=anomaly_range[0], x1=anomaly_range[1],
+            fillcolor='red', opacity=0.2,
+            layer="below", line_width=0)
+
+    fig.update_layout(xaxis_title="Date", yaxis_title="Values",
+                      xaxis={'showgrid': False},
+                      yaxis={'showgrid': True})
+    return fig
+
+def position_legend(fig, show_legend):
+    if show_legend:
+        fig.update_layout(legend=dict(
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=0.01
+        ))
+    else:
+        fig.update_layout(showlegend=show_legend)
+    return fig
+
+def change_labels(fig, sensor_labels_ext):
+    fig.for_each_trace(lambda t: t.update(name=sensor_labels_ext[t.name],
+        legendgroup=sensor_labels_ext[t.name],
+        hovertemplate=t.hovertemplate.replace(t.name, sensor_labels_ext[t.name])
+        )
+    )
+    return fig
